320f3ce52a9a498eaadc533d93073908c3c7a1f8
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-05-20 17:50:01 +0300

    add new function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def capital_letters(word) -> str:
+    """
+    Все буквы заглавные
+    """
     return word.upper()
+
+
+def word_title(word) -> str:
+    """
+    Преобразует каждое слово с заглавной буквы
+    """
+    return word.title()
